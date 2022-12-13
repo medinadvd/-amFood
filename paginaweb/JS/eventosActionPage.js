168bba1e9b91abf0745cbc1f9dea3e4b4d0a07ad
alpheta25 <116258535+alpheta25@users.noreply.github.com>
2022-12-13 13:41:58 +0100

    UploadFiles

--- paginaweb/JS/eventosActionPage.js
+++ paginaweb/JS/eventosActionPage.js
@@ -1,3 +1,5 @@
+const restaurante = document.querySelector('.restaurante');
+
 function rand(n){
     // creamos un numero al azar entre 1 y 10 (o cual sea la cantidad de imágenes)
      return(Math.floor(Math.random() * n + 1 ));
@@ -16,8 +18,45 @@ function rand(n){
         'imagenes/watermelon.png'
       ];
     function cambiar(){
-         document.getElementById("imagenuser").src = imagenes[rand(10)-1];
+        restaurante.setAttribute('hidden', true)
+        document.getElementById("imagenuser").src = imagenes[rand(10)-1];
+        
+        
     }
 
+    
+    const element = document.querySelector('.imagenLogo');
+    element.addEventListener("click", evento);
+
+    function evento() {
+      conexion();
+      restaurante.setAttribute('hidden', false)
+      element.remove();
+    }
 
     
+function conexion(){
+  fetch('https://jsonplaceholder.typicode.com/users')
+
+  .then(res => {
+     return res.json();
+  })
+  .then(data => {
+      data.forEach(user => {
+          const name = `${user.name}`; //cambiar name por la variable de la API del nombre del usuario
+          
+          document.querySelector('.user').insertAdjacentHTML('beforeend', name);
+
+      });
+  })
+  .catch(error => console.log(error));
+  
+   
+  
+}
+
+
+
+ 
+
+
