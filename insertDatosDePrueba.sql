1fcf6cf28442495fe57c4ecbe82b953513e4c76e
medinadvd <116387525+medinadvd@users.noreply.github.com>
2022-12-21 00:22:01 +0100

    Add files via upload

---
+++ insertDatosDePrueba.sql
@@ -0,0 +1,48 @@
+insert into restaurante values (1 ,1, "MARIO BROS", "Barcelona", "Italiana", 4, 93345123, "italiano.png");
+insert into preferencias_restaurante values (1 ,0, 0, 0, 0);
+insert into restaurante values (2 ,2, "MARIO BROS RETURN", "Barcelona", "Italiana", 4, 93123323, "italiano.png");
+insert into preferencias_restaurante values (2 ,1, 0, 0, 0); 
+insert into restaurante values (3 ,3, "MARIO BROS REVIVAL", "Barcelona", "Italiana", 4, 91245123, "italiano.png");
+insert into preferencias_restaurante values (3 ,0, 1, 0, 0); 
+
+insert into restaurante values (4 ,1, "CHIN CHON", "Barcelona", "China", 4, 93345123, "chino.png");
+insert into preferencias_restaurante values (4 ,1, 0, 0, 0);
+insert into restaurante values (5 ,2, "CHIN CHON RETURN", "Barcelona", "China", 4, 93123323, "chino.png");
+insert into preferencias_restaurante values (5 ,0, 1, 0, 0); 
+insert into restaurante values (6 ,3, "CHIN CHON REVIVAL", "Barcelona", "China", 4, 91245123, "chino.png");
+insert into preferencias_restaurante values (6 ,0, 0, 0, 1); 
+
+insert into restaurante values (7 ,1, "MILANESA PARTY", "Barcelona", "Argentina", 4, 93345123, "argentino.png");
+insert into preferencias_restaurante values (7 ,0, 1, 0, 0);
+insert into restaurante values (8 ,2, "MILANESA PARTY RETURN", "Barcelona", "Argentina", 4, 93123323, "argentino.png");
+insert into preferencias_restaurante values (8 ,0, 0, 1, 0); 
+insert into restaurante values (9 ,3, "MILANESA PARTY REVIVAL", "Barcelona", "Argentina", 4, 91245123, "argentino.png");
+insert into preferencias_restaurante values (9 ,0 , 0, 1, 0); 
+
+insert into restaurante values (10 ,1, "BAR PACO", "Barcelona", "Española", 4, 93345123, "español.png");
+insert into preferencias_restaurante values (10 ,0, 0, 1, 0);
+insert into restaurante values (11 ,2, "BAR PACO RETURN", "Barcelona", "Española", 4, 93123323, "español.png");
+insert into preferencias_restaurante values (11 ,0, 0, 0, 1); 
+insert into restaurante values (12 ,3, "BAR PACO REVIVAL", "Barcelona", "Española", 4, 91245123, "español.png");
+insert into preferencias_restaurante values (12 ,0, 1, 0, 0); 
+
+insert into restaurante values (13 ,1, "SHINIGAMI", "Barcelona", "Japonesa", 4, 93345123, "japones.png");
+insert into preferencias_restaurante values (13 ,0, 0, 0, 1);
+insert into restaurante values (14 ,2, "SHINIGAMI RETURN", "Japonesa", "Española", 4, 93123323, "japones.png");
+insert into preferencias_restaurante values (14 ,0, 1, 0, 0); 
+insert into restaurante values (15 ,3, "SHINIGAMI REVIVAL", "Japonesa", "Española", 4, 91245123, "japones.png");
+insert into preferencias_restaurante values (15 ,1, 0, 0, 0);
+
+insert into restaurante values (16 ,1, "MARIO BROS 2", "Barcelona", "Italiana", 4, 93345123, "italiano.png");
+insert into preferencias_restaurante values (16 ,0, 1, 1, 0);
+insert into restaurante values (17 ,2, "MARIO BROS RETURN 2", "Barcelona", "Italiana", 4, 93123323, "italiano.png");
+insert into preferencias_restaurante values (17 ,1, 0, 0, 1); 
+insert into restaurante values (18 ,3, "MARIO BROS REVIVAL 2", "Barcelona", "Italiana", 4, 91245123, "italiano.png");
+insert into preferencias_restaurante values (18 ,0, 1, 0, 0); 
+
+insert into restaurante values (19 ,1, "CHIN CHON 2", "Barcelona", "China", 4, 93345123, "chino.png");
+insert into preferencias_restaurante values (19 ,1, 1, 0, 0);
+insert into restaurante values (20 ,2, "CHIN CHON RETURN 2", "Barcelona", "China", 4, 93123323, "chino.png");
+insert into preferencias_restaurante values (20 ,0, 1, 0, 1); 
+insert into restaurante values (21 ,3, "CHIN CHON REVIVAL 2", "Barcelona", "China", 4, 91245123, "chino.png");
+insert into preferencias_restaurante values (21 ,0, 1, 0, 1); 
